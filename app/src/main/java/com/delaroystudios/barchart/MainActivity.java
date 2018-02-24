f2174541b111a0da883ebcb6ddc8c1f727fec90d
Delaroy Studios <bamoyk@yahoo.com>
2018-02-24 14:51:13 +0100

    Initial commit

---
+++ app/src/main/java/com/delaroystudios/barchart/MainActivity.java
@@ -0,0 +1,69 @@
+package com.delaroystudios.barchart;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+import com.delaroystudios.barchart.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BarChart barChart = (BarChart) findViewById(R.id.barchart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(8f, 0));
+        entries.add(new BarEntry(2f, 1));
+        entries.add(new BarEntry(5f, 2));
+        entries.add(new BarEntry(20f, 3));
+        entries.add(new BarEntry(15f, 4));
+        entries.add(new BarEntry(19f, 5));
+        entries.add(new BarEntry(8f, 6));
+        entries.add(new BarEntry(2f, 7));
+        entries.add(new BarEntry(5f, 8));
+        entries.add(new BarEntry(20f, 9));
+        entries.add(new BarEntry(15f, 10));
+        entries.add(new BarEntry(19f, 11));
+
+        BarDataSet bardataset = new BarDataSet(entries, "Cells");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Dec");
+        labels.add("Nov");
+        labels.add("Oct");
+        labels.add("Sep");
+        labels.add("Aug");
+        labels.add("Jul");
+        labels.add("Jun");
+        labels.add("May");
+        labels.add("Apr");
+        labels.add("Mar");
+        labels.add("Feb");
+        labels.add("Jan");
+
+        BarData data = new BarData(labels, bardataset);
+        barChart.setData(data); // set the data and list of lables into chart
+
+        barChart.setDescription("Expenditure for the year 2018");  // set the description
+
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        barChart.animateY(5000);
+
+
+    }
+}
